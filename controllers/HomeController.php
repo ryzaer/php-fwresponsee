7101cfb080b971582510921211df01863652e0b3
ryzaer <ryzaer@live.com>
2025-09-26 21:11:28 +0700

    add tailwind html

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -7,7 +7,8 @@ class HomeController extends \Router
         $this->set('title',$this->get('pwa.name'));
         $this->set('time_script',strtotime('now'));
         $this->set('base_script',$this->http->base);
-        print $this->render('templates/home.html');
+        print $this->render('templates/tailwind.html');
+        // print $this->render('templates/home.html');
     }
     
     function member($slug)
